0bb2049d7d19f5354d5b9c3a95292fdee3fadef0
InnovateITCaptain <152564216+InnovateITCaptain@users.noreply.github.com>
2025-02-14 21:24:17 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,5 +1,3 @@
-Script
-
 # This script check and untag the old docker images from the azure container registry
 # WARNING: this script will delete all image tags within a repository that share the same manifest
 
